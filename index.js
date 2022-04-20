3efa0b47624eadf536aa3b85387dc5c736ca2775
Marinou92 <44808159+Marinou92@users.noreply.github.com>
2022-04-20 21:33:42 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from "react-dom";
+import { Connection,clusterApiUrl, PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js';
+import App from "./App";
+ 
+ReactDOM.render(<App />,document.getElementById('root'));
